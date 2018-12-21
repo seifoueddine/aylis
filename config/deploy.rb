5e401292c8477b73f832c3c5a0b35b34c6c41f44
Seif Eddine NOUARA <nouaraseifeddine@gmail.com>
2018-12-22 00:46:10 +0100

    disable assets:precompile

--- config/deploy.rb
+++ config/deploy.rb
@@ -80,6 +80,9 @@ namespace :deploy do
 
 
   set :linked_dirs, fetch(:linked_dirs, []).push('public/system')
+  set :assets_dependencies, %w(app/assets lib/assets vendor/assets Gemfile.lock config/routes.rb)
+  Rake::Task["deploy:assets:precompile"].clear_actions
+  class PrecompileRequired < StandardError; end
 
 =begin
   namespace :paperclip do
