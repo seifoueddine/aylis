b9f30bdcc7e459afbfcc93ec083213623c0898aa
Seif Eddine NOUARA <nouaraseifeddine@gmail.com>
2018-12-28 19:46:08 +0100

    modification in nginx

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery prepend: true
+  protect_from_forgery with: :exception
+
 
   before_action :configure_devise, if: :devise_controller?
 
