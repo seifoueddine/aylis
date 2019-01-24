b51a5950bfcccceb14cce33a748accf8ca1cf130
Seif Eddine NOUARA <nouaraseifeddine@gmail.com>
2019-01-24 23:34:52 +0100

    fix host in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,7 +102,7 @@ Rails.application.configure do
       authentication: 'plain',
       enable_starttls_auto: true
   }
-  config.action_mailer.default_url_options = { host: "globlh.com" }
+  config.action_mailer.default_url_options = { host: "propartner.pro" }
 
 
 
