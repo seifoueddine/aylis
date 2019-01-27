91dc183b20fce9b8f703fc55086177f450fbc362
Seif Eddine NOUARA <nouaraseifeddine@gmail.com>
2019-01-27 14:28:07 +0100

    change port gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       domain: "gmail.com",
-      port: 587,
+      port: 465,
       user_name: "nouaraseifeddine@gmail.com",
       password: "Seaf123*",
       authentication: 'plain',
