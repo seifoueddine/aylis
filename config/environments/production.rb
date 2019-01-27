77364c4eee8292806e43ec82ff49a63ddb031e03
Seif Eddine NOUARA <nouaraseifeddine@gmail.com>
2019-01-27 14:07:19 +0100

    SMTP conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,9 +95,9 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
-      domain: "gmail.com",
+      domain: "lhpropartner.pro",
       port: 587,
-      user_name: "contact@lhpropartner.pro",
+      user_name: "contact",
       password: "contactlhpropartner",
       authentication: 'plain',
       enable_starttls_auto: true
