21f720b3d775f0958b3ef182057e1dea4bd8017f
Seif Eddine NOUARA <nouaraseifeddine@gmail.com>
2019-01-29 22:40:03 +0100

    add assets to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -79,11 +79,13 @@ namespace :deploy do
   end
 
 
+=begin
   set :linked_dirs, fetch(:linked_dirs, []).push('public/system')
    set :assets_dependencies, %w(app/assets lib/assets vendor/assets Gemfile.lock config/routes.rb)
    Rake::Task["deploy:assets:precompile"].clear_actions
    class PrecompileRequired < StandardError; end
 
+=end
 
 
   namespace :paperclip do
